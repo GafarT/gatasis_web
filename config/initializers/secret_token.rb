4bd3adad68343f06fa3a63a71d64bdf3192b0926
gafar <gafart@cvpolypackperkasa.com>
2013-08-09 01:41:28 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GatasisWeb::Application.config.secret_key_base = '13fd64091f2d9acd813fe85c7b5ff36dfcad89068d6e37f56439635f42494822af99e25b115758d22b6dbae859a581178fa974d1644fc53247375d8f9b870e63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GatasisWeb::Application.config.session_store :cookie_store, key: '_gatasis_web_session'
